e90180fbe190c43c107eae49663b4e5545fa4c39
Demivdm <demi.van.der.maarl@hva.nl>
2024-02-02 11:17:32 +0100

    🎉 ts set up

---
+++ ts-project/index.js
@@ -0,0 +1,4 @@
+console.log("hello world");
+// powershell is een kleine bitch en had eerst een andere policy nodig voor ik het script mocht uitvoeren
+var age = 20;
+// age os eem mumber dus er kan nu geen string aan toegevoegd worden
---
+++ ts-project/index.ts
@@ -0,0 +1,5 @@
+console.log("hello world")
+// powershell is een kleine bitch en had eerst een andere policy nodig voor ik het script mocht uitvoeren
+
+let age: number = 20;
+// age os eem mumber dus er kan nu geen string aan toegevoegd worden
